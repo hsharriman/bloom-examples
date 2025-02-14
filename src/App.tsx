2738ba4f02655a3f3b7ea722b5982dc0159755b8
hsharriman <hwei.shin.harriman@gmail.com>
2025-02-14 14:10:00 -0500

    added bare minimum default homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import { ConstructionsPage } from "./routes/Constructions";
+import { Header } from "./routes/Header";
 import { Home } from "./routes/Home";
 import { InteractiveExplanations } from "./routes/InteractiveExplanations";
 import { LabelCorrectionPage } from "./routes/Labels";
@@ -10,7 +11,8 @@ export const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navigate replace to="bloom-examples" />} />
-          <Route path="bloom-examples" element={<Home />}>
+          <Route path="bloom-examples" element={<Header />}>
+            <Route index element={<Home />} />
             <Route path="vegalite-labels" element={<LabelCorrectionPage />} />
             <Route path="explanations" element={<InteractiveExplanations />} />
             <Route path="constructions" element={<ConstructionsPage />} />
