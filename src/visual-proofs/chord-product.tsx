5f9457333b89e29e74d01f40cd2db1fe11a27a57
Griffin Teller <griffinteller@gmail.com>
2025-03-05 10:20:07 -0800

    chord products

--- src/visual-proofs/chord-product.tsx
+++ src/visual-proofs/chord-product.tsx
@@ -10,9 +10,10 @@ import {
   useDiagram,
   Vec2,
   mul,
-  ops, neg
+  ops, neg, div
 } from "@penrose/bloom";
 import {ChangeEvent, useEffect, useMemo, useState} from "react";
+import {Num} from "@penrose/core";
 
 const buildDiagram = async (numChords: number) => {
   const db = new DiagramBuilder(canvas(800, 400));
@@ -24,7 +25,9 @@ const buildDiagram = async (numChords: number) => {
     circle,
     ensure,
     build,
-    layer
+    layer,
+    equation,
+    bindToInput,
   } = db;
 
   const mainCircleRad = 150;
@@ -34,7 +37,7 @@ const buildDiagram = async (numChords: number) => {
     center: mainCircleCenter as Vec2,
     fillColor: [0, 0, 0, 0],
     strokeColor: [0, 0, 0, 1],
-    strokeWidth: 2,
+    strokeWidth: 3,
     r: mainCircleRad,
   });
 
@@ -46,24 +49,54 @@ const buildDiagram = async (numChords: number) => {
     r: 5,
     fillColor: [0, 0, 0, 1],
     drag: true,
-    dragConstraint: ([x, y]) => {
-      return [Math.min(mainCircleCenter[0], Math.max(mainCircleCenter[0] + mainCircleRad, x)), y];
+    dragConstraint: ([x]) => {
+      return [Math.max(mainCircleCenter[0], Math.min(mainCircleCenter[0] + mainCircleRad, x)), mainCircleCenter[1]];
     },
   })
 
+  let prod: Num = 1;
   for (let i = 0; i < numChords; i++) {
     const angle = 2 * Math.PI / numChords * i;
+    const delta = [Math.cos(angle) * mainCircleRad, Math.sin(angle) * mainCircleRad];
     const chord = line({
       start: draggableCenter.center,
-      end: [
-        mainCircleCenter[0] + Math.cos(angle) * mainCircleRad,
-        mainCircleCenter[1] + Math.sin(angle) * mainCircleRad,
-      ],
-      strokeWidth: 2,
+      end: ops.vadd(mainCircleCenter, delta) as Vec2,
+      strokeWidth: i == 0 ? 3 : 2,
+      strokeColor: i == 0 ? [1, 0, 0, 1] : [0, 0, 0, 1],
+    });
+
+    equation({
+      string: `\\zeta_{${i}}`,
+      center: ops.vadd(mainCircleCenter, ops.vmul(1.1, delta)) as Vec2,
     });
 
     layer(chord, draggableCenter);
+
+    if (i > 0) {
+      prod = mul(prod, div(ops.vdist(chord.end, chord.start), mainCircleRad));
+    }
   }
+
+  equation({
+    string: `\\lim_{z\\to\\zeta_0}\\frac{|z - \\zeta_0||z - \\zeta_1|\\ldots|z-\\zeta_{${numChords - 1}}|}{|z - \\zeta_0|}`,
+    center: [200, 100]
+  });
+
+  equation({
+    string: `= \\lim_{z\\to 1}\\frac{\\left|z^{n+1}-1\\right|}{|z - 1|}`,
+    center: [139, 50]
+  });
+
+  equation({
+    string: `= \\lim_{z\\to 1}1 + z + \\ldots + z^{${numChords - 1}}`,
+    center: [155, 0]
+  });
+  equation({
+    string: `= ${numChords}`,
+    center: [94.5, -50]
+  });
+
+  bindToInput(prod, { name: "product" });
 
   return await build();
 }
@@ -71,10 +104,15 @@ const buildDiagram = async (numChords: number) => {
 export default function ChordProductDiagram() {
   const [diagram, setDiagram] = useState<Diagram | null>(null);
   const [numChords, setNumChords] = useState<number>(6);
+  const [product, setProduct] = useState(0);
 
   useEffect(() => {
     buildDiagram(numChords).then(setDiagram);
   }, [numChords]);
+
+  useEffect(() => {
+    diagram?.addInputEffect("product", setProduct);
+  }, [diagram]);
 
   const onSliderChange = useMemo(() => (e: ChangeEvent) => {
     setNumChords(Number.parseInt((e.target as HTMLInputElement).value));
@@ -85,6 +123,7 @@ export default function ChordProductDiagram() {
       <div style={{ width: "50em" }}>
         <Renderer diagram={diagram} />
       </div>
+      <p>product: {product}</p>
       <input style={{ margin: "auto", width: "100%" }} type={"range"} min={2} max={20} onChange={onSliderChange} value={numChords}/>
     </div>
   );
